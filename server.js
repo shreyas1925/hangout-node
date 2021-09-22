d3e84fdf1df537866270ca5165b573ddf645aa8d
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-09-22 14:59:24 +0530

    ✅ adding login routes

--- server.js
+++ server.js
@@ -4,11 +4,25 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const { v4: uuidV4 } = require("uuid");
 
+const session = require("express-session");
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "SECRET",
+  })
+);
+
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/login", (req, res) => {
+  res.render("pages/auth");
 });
 
 app.get("/:room", (req, res) => {
@@ -29,3 +43,60 @@ io.on("connection", (socket) => {
 server.listen(3000, () => {
   console.log("Running on port 3000...");
 });
+
+/*  PASSPORT SETUP  */
+
+const passport = require("passport");
+var userProfile;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set("view engine", "ejs");
+
+app.get("/success", (req, res) =>
+  res.render("pages/success", { user: userProfile })
+);
+app.get("/error", (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+/*  Google AUTH  */
+
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const GOOGLE_CLIENT_ID =
+  "850395898530-5d2nm004c8arv1di825f5sjelb8fjmpd.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "yIhY83XGKUda4tAarC_SEd_R";
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      userProfile = profile;
+      return done(null, userProfile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/error" }),
+  function (req, res) {
+    // Successful authentication, redirect success.
+    res.redirect("/success");
+  }
+);
